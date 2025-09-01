ebc0d5d2ba5edf11098f3831b46a421ed67d880b
mguo <mguohome@gmail.com>
2025-09-01 14:05:09 -0400

    ready to prod

--- src/main/java/fisher/fishing/FishingController.java
+++ src/main/java/fisher/fishing/FishingController.java
@@ -4,33 +4,30 @@ import fisher.util.FileUtils;
 import fisher.input.Clicker;
 
 public class FishingController {
-    private static final double MIN_TIME_BETWEEN_DETECTIONS = 1.5; // seconds
+    private static final double MIN_TIME_BETWEEN_DETECTIONS_MS = 1500;
+    private static final double MIN_TIME_BETWEEN_ABILITY_CASTS_MS = 3000;
     private static long lastDetectionTime = System.currentTimeMillis();
+    private static long lastAbilityCastTime = System.currentTimeMillis();
 
     public static void onDetect() {
         if (!State.getInstance().isRunning()) {
             return;
         }
         long now = System.currentTimeMillis();
-        double nowSeconds = now / 1000.0;
-        double lastDetectionSeconds = lastDetectionTime / 1000.0;
-        double timeSinceLastDetection = nowSeconds - lastDetectionSeconds;
 
-        if (timeSinceLastDetection > MIN_TIME_BETWEEN_DETECTIONS) {
+        if (now - lastDetectionTime > MIN_TIME_BETWEEN_DETECTIONS_MS) {
             lastDetectionTime = now;
 
             FileUtils.writeStringToFile("[LuluTheFish] detected note block!!");
-            if (State.getInstance().inAbilityMode()) {
+            if (State.getInstance().inAbilityMode()
+                    && (now - lastAbilityCastTime > MIN_TIME_BETWEEN_ABILITY_CASTS_MS)) {
+                lastAbilityCastTime = now;
                 Clicker.getInstance().scheduleReelAbilityAndRecast();
 
             } else {
                 Clicker.getInstance().scheduleReelAndRecast();
             }
 
-        } else {
-            FileUtils.writeStringToFile(
-                    String.format("[LuluTheFish] Warning!! detected duplicate note block again in only %.3f",
-                            timeSinceLastDetection));
         }
     }
 }
